c9b830a60c3160226bfd9c659adece2ebde2ff70
katiescruggs <katie.e.scruggs@gmail.com>
2017-12-13 10:19:05 -0700

    connect Redux components together

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
+import { NavLink, Link, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import apiKey from '../../apiKey.js';
+import { fetchMovies } from '../../actions'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  async componentDidMount() {
+    const movieResponse = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`);
+    const movies = await movieResponse.json();
+
+
+    //dispatch action to set the store
+    this.props.handleFetch(movies)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    handleFetch: (movies) => {
+      dispatch(fetchMovies(movies))
+    }
+  }
+}
+
+
+export default connect(null, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import rootReducer from './reducers'
+const store = createStore(rootReducer)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const route = (
+  <Provider store = {store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+)
+
+ReactDOM.render(route, document.getElementById('root'));
 registerServiceWorker();
