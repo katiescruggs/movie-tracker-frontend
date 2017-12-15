73f378331b10936336465fd272a490c34019591d
katiescruggs <katie.e.scruggs@gmail.com>
2017-12-15 11:06:49 -0700

    break out app into routes and header components

---
+++ src/components/Routes/Routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import CardContainer from '../../containers/CardContainer/CardContainer';
+import Login from '../../containers/Login/Login';
+
+const Routes = () => {
+  return (
+    <div className="routes">
+        <Route 
+          exact path='/login'  
+          render={props => <Login {...props} />}
+        />
+        <Route 
+          exact path='/register'  
+          render={props => <Login {...props} showRegister />}
+        />
+        <Route exact path='/' component={CardContainer} />
+    </div>
+  );
+}
+
+export default Routes;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,73 +1,20 @@
 import React, { Component } from 'react';
-import { Link, Route, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import CardContainer from '../CardContainer/CardContainer';
-import Login from '../Login/Login';
-import { fetchMovies } from '../../actions';
+import Header from '../Header/Header.js';
 import PropTypes from 'prop-types';
+import Routes from '../../components/Routes/Routes.js';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  async componentDidMount() {
-    this.props.handleFetch();
-  }
-
   render() {
-    const name = this.props.name ? `${this.props.name}'s` : '';
-
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">{`${name} MovieTracker`}</h1>
-          <Link to='/login'>
-            <button className='btn-log-in'>
-              Log In
-            </button>
-          </Link>
-          <Link to='/register'>
-            <button className='btn-sign-up'>
-              Sign Up
-            </button>
-          </Link> 
-        </header>
-
-        <Route 
-          exact path='/login'  
-          render={props => <Login {...props} />}
-        />
-        <Route 
-          exact path='/register'  
-          render={props => <Login {...props} showRegister />}
-        />
-        <Route exact path='/' component={CardContainer} />
+        <Header />
+        <Routes />
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    name: state.user.info.name
-  };
-};
 
-const mapDispatchToProps = dispatch => {
-  return {
-    handleFetch: () => {
-      dispatch(fetchMovies());
-    }
-  };
-};
-
-App.propTypes = {
-  name: PropTypes.string,
-  handleFetch: PropTypes.func
-};
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+//export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default App;
