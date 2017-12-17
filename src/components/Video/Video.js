562f350c6d268e607475696845b60045483fe5fd
Jesse Packwood <packwoodjustin@gmail.com>
2017-12-17 16:34:30 -0700

    change entry video

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -4,7 +4,7 @@ import './Video.css'
 const FilmReel = () => {
   return (
       <div className="film-reel-container">
-      <iframe src="https://www.youtube.com/embed/UbzKuFnXxiU?rel=0&amp;autoplay=1&amp;controls=0&amp;showinfo=0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen autoplay></iframe>
+      <iframe src="https://www.youtube.com/embed/9RTaIpVuTqE?rel=0&amp;autoplay=1&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
       </div>
   )
 }
