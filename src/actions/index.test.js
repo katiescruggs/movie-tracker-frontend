65851b44277bae270bf3a43d6809febb5e259f4c
katiescruggs <katie.e.scruggs@gmail.com>
2017-12-17 19:31:24 -0700

    apiCalls tests from apiCalls file complete

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -5,34 +5,34 @@ describe('all actions', () => {
     let userInfo;
     beforeEach( () => {
       userInfo = {id: 1, name: 'Yung Jhun', email: 'Jhun@gmail.com', password: 'password'}
-    })
+    });
     it('has a type of USER_LOGIN_SUCCESS', () => {
       const expected = {
         type: 'USER_LOGIN_SUCCESS',
         userInfo
-      }
+      };
       expect(actions.userLoginSuccess(userInfo)).toEqual(expected)
-    })
+    });
 
     it('has a type of USER_LOGIN_ERROR', () => {
       const expected = {
         type: 'USER_LOGIN_ERROR',
         userInfo
-      }
-    })
+      };
+    });
 
     it('has a type of userSignUpError', () => {
       const expected = {
       type: 'USER_SIGNUP_ERROR',
       errorMessage: 'SIGN UP ERROR'
-      }
-      expect(actions.userSignUpError()).toEqual(expected)
-    })
+      };
+      expect(actions.userSignUpError()).toEqual(expected);
+    });
 
     it('should dispatch a sign up success or error', () => {
 
-    })
-  })
+    });
+  });
 
   describe('movie actions', () => {
     it('has a type of SET_MOVIES', () => {
@@ -40,36 +40,35 @@ describe('all actions', () => {
       const expected = {
         type: 'SET_MOVIES',
         movies
-      }
+      };
       expect(actions.setMovies(movies)).toEqual(expected)
-    })
-    
-  })
+    });  
+  });
+
   describe('favorite actions', () => {
     it('should have a type of SET_FAVORITES', () => {
-      const favorites = [{ title: 'Casper' }, { title: 'Thor' }, { title: 'Batman' }]
+      const favorites = [{ title: 'Casper' }, { title: 'Thor' }, { title: 'Batman' }];
       const expected = {
         type: 'SET_FAVORITES',
         favorites
-      }
-    })
+      };
+    });
 
     it('should have a type of CLEAR_FAVORITES', () => {
-      const favorites = [{ title: 'Casper' }, { title: 'Thor' }, { title: 'Batman' }]
+      const favorites = [{ title: 'Casper' }, { title: 'Thor' }, { title: 'Batman' }];
       const expected = {
         type: 'CLEAR_FAVORITES',
         favorites
-      }
-    })
+      };
+    });
 
 
     it('should add favorites', () => {
 
-    })
+    });
 
     it('should remove favorites', () => {
 
-    })
-  })
-
+    });
+  });
 });
