e2a8d3b6159f40e334859614f0c059b3d328ab4f
katiescruggs <katie.e.scruggs@gmail.com>
2017-12-18 07:10:49 -0700

    add one more test to favoritesReducer

--- src/reducers/favoritesReducer.test.js
+++ src/reducers/favoritesReducer.test.js
@@ -8,11 +8,17 @@ describe('favoritesReducer tests', () => {
     expect(favoritesReducer(undefined, {})).toEqual(expected);
   });
 
-  it('should return a new state, with a favorite', () => {
+  it('setFavorites should return a new state, with a favorite', () => {
     const favorites = [{title: 'Star Wars', overview: 'Lightsabers'}];
     const expected = [...favorites];
     expect(favoritesReducer(undefined, actions.setFavorites(favorites))).
       toEqual(expected);
   });
 
+  it('clearFavorites should return state of an empty array', () => {
+    const action = actions.clearFavorites();
+    const expected = [];
+
+    expect(favoritesReducer(undefined, action)).toEqual(expected);
+  });
 });
