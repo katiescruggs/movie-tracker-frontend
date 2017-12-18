22f5d8d417b3b9b0a4d1329543e380c3f7807462
katiescruggs <katie.e.scruggs@gmail.com>
2017-12-17 23:04:38 -0700

    delete redundant files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App/App';
+import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
